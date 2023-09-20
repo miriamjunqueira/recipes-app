6e6b1ead0f780b0f33e09a7dbc347e6cf5623fe8
Thiago Pedrosa <thiagopedrosaw@gmail.com>
2023-09-20 17:23:08 -0300

    Req 4

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const INITIAL_DATA_USER = {
+  email: '',
+  password: '',
+};
 
 export default function Login() {
+  const [loginData, setLoginData] = useState(INITIAL_DATA_USER);
+
+  const inputsValidation = (email: string, password: string) => {
+    const regex = /^\S+@\S+\.\S+$/;
+    return regex.test(email) && password.length > 6;
+  };
+
+  const isDisabled = inputsValidation(loginData.email, loginData.password);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginData({
+      ...loginData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div>
       <form>
-        <input type="email" data-testid="email-input" />
-        <input type="password" data-testid="password-input" />
-        <button data-testid="login-submit-btn">Enter</button>
+        <input
+          type="email"
+          data-testid="email-input"
+          name="email"
+          onChange={ handleChange }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          name="password"
+          onChange={ handleChange }
+        />
+        <button data-testid="login-submit-btn" disabled={ !isDisabled }>Enter</button>
       </form>
+      <div>{loginData.email}</div>
     </div>
   );
 }
--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-test('Farewell, front-end', () => {
-  // Este arquivo pode ser modificado ou deletado sem problemas
-  render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+test('Testa se os inputs e o botão estão na tela', () => {
+  renderWithRouter(<App />);
+  const emailInput = screen.getByTestId('email-input');
+  const passwordInput = screen.getByTestId('password-input');
+  const enterButton = screen.getByTestId('login-submit-btn');
+  expect(emailInput && passwordInput && enterButton).toBeInTheDocument();
+});
+
+test('Testa se os inputs e o botão estão na tela', async () => {
+  const { user } = renderWithRouter(<App />);
+  const emailInput = screen.getByTestId('email-input');
+  await user.type(emailInput, 'teste@teste.com');
+  const email = screen.getByText(/teste@teste\.com/i);
+  expect(email).toBeInTheDocument();
 });
