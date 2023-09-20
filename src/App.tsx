534b2d77d45b463c14b06512da8e6a529b1b4838
Thiago Pedrosa <thiagopedrosaw@gmail.com>
2023-09-20 16:27:22 -0300

    Resolve lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
+import './App.css';
+// import rockGlass from './images/rockGlass.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Pages/Login/Login';
 
 function App() {
