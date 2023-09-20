1d82149447354909a313518b1fb779e4c5527599
Thiago Pedrosa <thiagopedrosaw@gmail.com>
2023-09-20 16:13:57 -0300

    Requisito 2 e 3

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,14 @@ import React from 'react';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">Recipes G19</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+    </Routes>
   );
 }
 
