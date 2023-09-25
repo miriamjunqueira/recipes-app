f70740a99f8de1dfdc20e61f72bd311a89490fb6
Thiago Pedrosa <thiagopedrosaw@gmail.com>
2023-09-25 20:05:34 -0300

    Resvolve Testes para 12

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,11 +1,14 @@
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
+import { vi } from 'vitest';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
+import * as APIModules from '../Services/API';
 
 const emailTestId = 'email-input';
 const emailTest = 'teste@teste.com';
 const passwordTestId = 'password-input';
 const loginTestId = 'login-submit-btn';
+const filterButton = 'search-top-btn';
 
 describe('Teste do AppReceitas', () => {
   test('Testa se os inputs e o botão estão na tela', () => {
@@ -45,15 +48,33 @@ describe('Teste do AppReceitas', () => {
     await user.type(passwordInput, '1234567');
     await user.click(enterButton);
     const profileButton = screen.getByTestId('profile-top-btn');
-    const searchButton = screen.getByTestId('search-top-btn');
+    const searchButton = screen.getByTestId(filterButton);
     await user.click(profileButton);
     expect(searchButton).not.toBeInTheDocument();
   });
   test('Testa se ao clicar no botão de pesquisa o input de texto aparece para digitar', async () => {
     const { user } = renderWithRouter(<App />, { route: '/meals' });
-    const searchButton = screen.getByTestId('search-top-btn');
+    const searchButton = screen.getByTestId(filterButton);
     await user.click(searchButton);
     const searchInput = screen.getByTestId('search-input');
     expect(searchInput).toBeInTheDocument();
+  });
+  test('Testa se a rota Drinks possui o texto Drinks na tela', () => {
+    renderWithRouter(<App />, { route: '/drinks' });
+    const drinksText = screen.getByTestId('page-title');
+    expect(drinksText).toBeInTheDocument();
+  });
+  test('Testa se a requisição da API é feita', async () => {
+    const { user } = renderWithRouter(<App />, { route: '/drinks' });
+    const filterButtonToClick = screen.getByTestId(filterButton);
+    await user.click(filterButtonToClick);
+    const searchInput = screen.getByTestId('search-input');
+    await user.type(searchInput, 'chicken');
+    const searchButton = screen.getByTestId('exec-search-btn');
+    await user.click(searchButton);
+    waitFor(() => {
+      vi.spyOn(APIModules, 'default');
+      expect(APIModules).toHaveBeenCalled();
+    });
   });
 });
