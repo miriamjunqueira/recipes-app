0b48eb1d2ce04bbc33fbc67fa2ff279e26438e9f
Thiago Pedrosa <thiagopedrosaw@gmail.com>
2023-09-29 20:28:33 -0300

    Resolve Req 31

--- src/Pages/RecipeDetails/DetailsButton.tsx
+++ src/Pages/RecipeDetails/DetailsButton.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
 export default function DetailsButton() {
+  const { pathname } = useLocation();
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const inProgressRecipesString = localStorage.getItem('inProgressRecipes');
   const getInProgressInfo = inProgressRecipesString
@@ -17,10 +19,15 @@ export default function DetailsButton() {
   const isInProgress = inProgressDrinks || inProgressMeals
     ? 'Continue Recipe' : 'Start Recipe';
 
+  const handleClick = (path: string) => {
+    navigate(`${path}/in-progress`);
+  };
+
   return (
     <button
       className="start-button"
       data-testid="start-recipe-btn"
+      onClick={ () => handleClick(pathname) }
     >
       {isInProgress}
     </button>
