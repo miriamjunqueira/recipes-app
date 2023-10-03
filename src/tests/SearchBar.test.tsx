c1eb2858fbce664f5cc3728d24d70e13556bedbc
Thiago Pedrosa <thiagopedrosaw@gmail.com>
2023-10-03 19:50:11 -0300

    Resolve erro de lint

--- src/tests/SearchBar.test.tsx
+++ src/tests/SearchBar.test.tsx
@@ -1,4 +1,4 @@
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { vi } from 'vitest';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
